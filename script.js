69974ee1f12ad8e751abe75e53656f60a327cd0e
Tokancz <matyasklubal@gmail.com>
2025-04-24 08:23:55 +0200

    supabase neee

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-import { createClient } from '/@supabase/supabase-js'
+import { createClient } from '/node_modules/@supabase/supabase-js/';
 
-const supabaseUrl = 'https://mlyroqzufldgnolfejac.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
+const supabaseUrl = 'https://mlyroqzufldgnolfejac.supabase.co';
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
